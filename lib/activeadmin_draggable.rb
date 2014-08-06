818763a38c5088dbad9a113e30e3c8a38e76d927
wild <wild@portal>
2014-08-06 15:24:27 +0400

    move creation of collection_action and button to dsl method

--- lib/activeadmin_draggable.rb
+++ lib/activeadmin_draggable.rb
@@ -2,6 +2,7 @@ require 'activeadmin_draggable/version'
 require 'activeadmin_draggable/engine'
 require 'activeadmin_draggable/draggable_list_item'
 require 'activeadmin_draggable/sortable_tree'
+require 'activeadmin/resource_dsl'
 
 module ActiveadminDraggable
 end
